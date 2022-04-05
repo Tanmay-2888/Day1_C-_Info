ad2d641d12221a938b3c4cf590c50942837a5bdc
Tanmay-2888 <103040016+Tanmay-2888@users.noreply.github.com>
2022-04-05 17:27:41 +0530

    commit

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace Day1_C__Info
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Test");
         }
     }
 }
