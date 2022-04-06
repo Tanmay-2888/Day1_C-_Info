1bbbc419a78accf4c4ba81022dd2b70dffcbca25
Tanmay-2888 <103040016+Tanmay-2888@users.noreply.github.com>
2022-04-06 16:39:28 +0530

    commit

--- Program.cs
+++ Program.cs
@@ -6,14 +6,56 @@ namespace Day1_C__Info
     {
         static void Main(string[] args)
         {
-           int a=10;
+          /* int a=10;
            float f=23.45f;
            double d= 56.677;
            string str ="Tanmay";
            System.Console.WriteLine("value of a ="+a);
            System.Console.WriteLine("value of f {0} , d={1}",f,d);
            System.Console.WriteLine($"a={a}, float ={f}, double={d}");
-           System.Console.WriteLine(str);
+           System.Console.WriteLine(str);*/
+
+          /* int Addition, substraction, multiplication, division;
+
+           System.Console.WriteLine("Enter First number: ");
+           int num1=Convert.ToInt32(Console.ReadLine());
+           System.Console.WriteLine("Enter another Number");
+           int num2=Convert.ToInt32(Console.ReadLine());
+
+           Addition= num1 + num2;
+           System.Console.WriteLine(Addition);
+
+           substraction= num1 - num2;
+           System.Console.WriteLine(substraction);
+
+           multiplication= num1 * num2;
+           System.Console.WriteLine(multiplication);
+
+           division= num1/ num2;
+           System.Console.WriteLine(division);*/
+
+          /* System.Console.WriteLine("Enter Age");
+           int age =Convert.ToInt32(Console.ReadLine());
+           if(age>18)
+           {
+               System.Console.WriteLine("Valid for voting");
+           }
+           else{
+               Console.WriteLine("Not valid for voting");
+           }*/
+
+
+
+           System.Console.WriteLine("Enter Age");
+           int age =Convert.ToInt32(Console.ReadLine());
+           if(age>18)
+           {
+               System.Console.WriteLine("Valid for voting");
+           }
+           else{
+               Console.WriteLine("Not valid for voting");
+           }
+
         }
     }
 }
